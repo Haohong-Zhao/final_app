622b9cd91376b2dc24cd9408d2d974fb88a82a25
Rijish Ganguly <rijish96@gmail.com>
2019-01-29 21:40:13 -0500

    Added three components

--- taxi-ui/src/app/app.module.ts
+++ taxi-ui/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { LogInComponent } from './components/log-in/log-in.component';
+import { LandingComponent } from './components/landing/landing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignUpComponent,
+    LogInComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule
